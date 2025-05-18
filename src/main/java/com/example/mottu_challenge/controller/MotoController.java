f25951ba6f533bb136ff2431538d0799517c96ef
Nathália Gomes <gmsssnathalia@gmail.com>
2025-05-18 10:06:08 -0300

    feat: adicionando listar paginando

--- src/main/java/com/example/mottu_challenge/controller/MotoController.java
+++ src/main/java/com/example/mottu_challenge/controller/MotoController.java
@@ -4,8 +4,13 @@ import com.example.mottu_challenge.service.MotoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/motos")
@@ -24,7 +29,7 @@ public class MotoController {
         return motoService.buscarPorId(id);
     }
 
-    @GetMapping
+    @GetMapping("/custom")
     public ResponseEntity<Page<MotoDTO>> listar(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size,
@@ -33,6 +38,20 @@ public class MotoController {
         Page<MotoDTO> motos = motoService.listarPaginado(page, size, sortBy);
         return ResponseEntity.ok(motos);
     }
+
+    @GetMapping
+    public ResponseEntity<Page<MotoDTO>> listarTodos(
+            @PageableDefault(size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
+        return ResponseEntity.ok(motoService.listarPaginado(pageable));
+    }
+
+    @GetMapping("/buscar")
+    public ResponseEntity<List<MotoDTO>> buscarPorStatusOuModelo(
+            @RequestParam(required = false) String status,
+            @RequestParam(required = false) String modelo) {
+        return ResponseEntity.ok(motoService.buscarPorStatusOuModelo(status, modelo));
+    }
+
 
     @PutMapping("/{id}")
     public MotoDTO atualizar(@PathVariable Long id, @Valid @RequestBody MotoDTO dto) {
@@ -43,4 +62,10 @@ public class MotoController {
     public void excluir(@PathVariable Long id) {
         motoService.excluir(id);
     }
+
+    @GetMapping("/paginado")
+    public ResponseEntity<Page<MotoDTO>> listarPaginado(Pageable pageable) {
+        return ResponseEntity.ok(motoService.listarPaginado(pageable));
+    }
+
 }
